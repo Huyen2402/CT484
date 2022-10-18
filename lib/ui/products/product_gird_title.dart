0d8f6a98edaf29282aa69d9f9e63f89838674c91
Huyen2402 <huyenb1910384@student.ctu.edu.vn>
2022-10-18 08:55:39 +0700

    Cai dat chuc nang chon san pham yeu thich

--- lib/ui/products/product_gird_title.dart
+++ lib/ui/products/product_gird_title.dart
@@ -3,7 +3,7 @@ import '../../model/product.dart';
 import 'products_detail_screen.dart';
 
 class ProductGirdTitle extends StatelessWidget {
-  const ProductGirdTitle(this.product,{super.key});
+  const ProductGirdTitle(this.product, {super.key});
   final Product product;
 
   @override
@@ -13,11 +13,9 @@ class ProductGirdTitle extends StatelessWidget {
       child: GridTile(
         footer: buildGirdFooterBar(context),
         child: GestureDetector(
-          onTap: (){
-           Navigator.of(context).pushNamed(
-           ProductDetailScreen.routeName,
-           arguments: product.id
-           );
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
           },
           child: Image.asset(
             product.imageUrl,
@@ -27,27 +25,33 @@ class ProductGirdTitle extends StatelessWidget {
       ),
     );
   }
-  Widget buildGirdFooterBar(BuildContext context){
+
+  Widget buildGirdFooterBar(BuildContext context) {
     return GridTileBar(
       backgroundColor: Colors.black87,
-      leading: IconButton(
-        icon: Icon(
-          product.isFavorite ? Icons.favorite : Icons.favorite_border,
-        ),
-        color: Theme.of(context).colorScheme.secondary,
-        onPressed: (){
-          print('Toggle a favorite product');
+      leading: ValueListenableBuilder<bool>(
+        valueListenable: product.isFavoriteListenable,
+        builder: (ctx, isFavorite, child) {
+          return IconButton(
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              product.isFavorite = !isFavorite;
+            },
+          );
         },
       ),
       title: Text(
         product.title,
         textAlign: TextAlign.center,
       ),
-      trailing:  IconButton(
+      trailing: IconButton(
         icon: const Icon(
           Icons.shopping_cart,
         ),
-        onPressed: (){
+        onPressed: () {
           print('Add item to cart');
         },
         color: Theme.of(context).colorScheme.secondary,
